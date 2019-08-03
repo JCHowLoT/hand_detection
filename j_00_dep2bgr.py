b1b7a65a767efb9f26cfd87fca7ac766c2f15c3d
JCHowLoT <50939824+JCHowLoT@users.noreply.github.com>
2019-08-03 21:51:18 +0800

    Update j_00_dep2bgr.py

--- j_00_dep2bgr.py
+++ j_00_dep2bgr.py
@@ -1,7 +1,8 @@
 import cv2
 import os
 
-
+# depth image directory path
+# save image directory path
 data_path = '/media/j/DA18EBFA09C1B27D/1/dep_r1000_n357'
 save_path = '/media/j/DA18EBFA09C1B27D/1/gbr_r1000_n357'
 filename = os.listdir(data_path)
@@ -12,7 +13,10 @@ for fname in filename:
     path = data_path + "/" + fname
     save_path = save_path
     im_gray = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    
+    # Adjust the parameter '16' to make the image look better
     im_gray = im_gray * 16
+    
     im_color = cv2.applyColorMap(im_gray, cv2.COLORMAP_JET)
     cv2.imshow('img', im_color)
     save_img = save_path + "/" + fname
